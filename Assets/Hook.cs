31a26cfc38ad5a89dba3cc64e5b55435402dbd8c
Jado <jade.bouiges@edu.esiee-it.fr>
2024-09-25 11:26:11 +0200

    add hook mecanic

---
+++ Assets/Hook.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hook : MonoBehaviour
+{
+    private PlayerController _playerController;
+
+    private void Awake()
+    {
+        _playerController = FindObjectOfType<PlayerController>().gameObject.GetComponent<PlayerController>();
+    }
+    public void Initialize(PlayerController playerController)
+    {
+        _playerController = playerController;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("GrappleSurface"))
+        {
+            Vector3 hitPoint = collision.contacts[0].point;
+            _playerController.StartGrapple(hitPoint);
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,20 +5,94 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public Rigidbody2D rb;
+    public Rigidbody rb;
+    public Camera _myCam;
 
     private float _horizontal;
     private float _speed = 8f;
     private float _jumpingPower = 25f;
-    void Start()
-    {
-        
-    }
+
+    [SerializeField] private GameObject _circle;
+    [SerializeField] private float _circleRadius = 2f;
+
+    [SerializeField] private GameObject _hookPrefab; 
+    [SerializeField] private float _hookSpeed = 20f; 
+    [SerializeField] private float _grappleSpeed = 10f;
+    [SerializeField] private float _grappleDistanceMargin = 1f; 
+
+    private GameObject _spawnHook;
+    private Vector3 _grapplePoint; 
+    private bool _isGrappling = false;
 
     void Update()
     {
-        rb.velocity = new Vector2(_horizontal * -_speed, rb.velocity.y);
+        if (!_isGrappling)
+        {
+            rb.velocity = new Vector2(_horizontal * -_speed, rb.velocity.y);
+        }
+
+        FollowMouseWithCircle();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Hook();
+        }
+
+        if (_isGrappling)
+        {
+            Grapple();
+        }
+    }
+
+    private void FollowMouseWithCircle()
+    {
+        Ray ray = _myCam.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.forward, transform.position);
+        float distance;
+
+        if (plane.Raycast(ray, out distance))
+        {
+            Vector3 cursorWorldPosition = ray.GetPoint(distance);
+            Vector3 direction = (cursorWorldPosition - transform.position).normalized;
+            Vector3 circlePosition = transform.position + direction * _circleRadius;
+            _circle.transform.position = circlePosition;
+        }
+    }
+
+    private void Hook()
+    {
+        _spawnHook = Instantiate(_hookPrefab, _circle.transform.position, _circle.transform.rotation);
+
+        Rigidbody hookRb = _spawnHook.GetComponent<Rigidbody>();
+        if (hookRb == null)
+        {
+            hookRb = _spawnHook.AddComponent<Rigidbody>();
+        }
+
+        Vector3 direction = (_circle.transform.position - transform.position).normalized;
+
+        hookRb.velocity = direction * _hookSpeed;
+
+        _spawnHook.AddComponent<Hook>().Initialize(this);
+    }
+
+    public void StartGrapple(Vector3 hitPoint)
+    {
+        _grapplePoint = hitPoint;
+        _isGrappling = true;
+    }
+
+    private void Grapple()
+    {
+        Vector3 direction = (_grapplePoint - transform.position).normalized;
+
+        rb.velocity = direction * _grappleSpeed;
+
+        if (Vector3.Distance(transform.position, _grapplePoint) < _grappleDistanceMargin)
+        {
+            _isGrappling = false;
+            rb.velocity = Vector2.zero; 
+        }
     }
 
     public void OnJump(InputAction.CallbackContext context)
@@ -28,7 +102,7 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, _jumpingPower);
         }
 
-        if(context.canceled && rb.velocity.y > 0f)
+        if (context.canceled && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
