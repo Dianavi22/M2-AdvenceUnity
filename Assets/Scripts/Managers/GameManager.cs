dc0947880fc4266a1a856dacd00fa1934b9f199d
Jado <jade.bouiges@edu.esiee-it.fr>
2024-11-26 17:19:54 +0100

    clean gameManager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] int _maxItem = 50;
-    [SerializeField] Timer _timer;
     public int currentNumberItem;
     public int nbDeath;
+    [SerializeField] int _maxItem = 50;
+    [SerializeField] GameObject _player;
+
+    [Header("References")]
+    [SerializeField] DataManager _dataManager;
+    [SerializeField] ShakyCame _shakyCame;
+    [SerializeField] Timer _timer;
+
+    [Header("Particules")]
+    [SerializeField] GameObject _victoryPart;
+    [SerializeField] GameObject _playerVictoryPart;
+
+    [Header("UI")]
+    [SerializeField] GameObject _victoryCanvas;
     [SerializeField] TMP_Text _nbItemText;
     [SerializeField] TMP_Text _scoreText;
     [SerializeField] TMP_Text _scoreDeathText;
     [SerializeField] TMP_Text _hightScore;
     [SerializeField] TMP_Text _hightDeathScore;
-    [SerializeField] GameObject _victoryCanvas;
-    [SerializeField] GameObject _player;
-    [SerializeField] DataManager _dataManager;
     [SerializeField] TMP_Text _newRecordTxt;
     [SerializeField] TMP_Text _newRecordDeathTxt;
-    [SerializeField] ShakyCame _shakyCame;
-    [SerializeField] GameObject _victoryPart;
-    [SerializeField] GameObject _playerVictoryPart;
 
 
     public bool isFinish;
@@ -40,6 +46,7 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    #region Victory
     private IEnumerator VictoryEffect()
     {
         isFinish = true;
@@ -50,15 +57,12 @@ public class GameManager : MonoBehaviour
         _playerVictoryPart.SetActive(true);
         yield return new WaitForSeconds(5);
         _shakyCame.enabled = false;
-
-
         Victory();
 
     }
 
-    public void Victory()
+    private void PlayerPrefsSetUp()
     {
-
         if (_timer.seconds <= _dataManager.hightScore)
         {
             _dataManager.hightScore = _timer.seconds;
@@ -73,14 +77,23 @@ public class GameManager : MonoBehaviour
             _newRecordDeathTxt.gameObject.SetActive(true);
         }
         PlayerPrefs.SetInt("Finished", 1);
+    }
+
+    public void Victory()
+    {
+        PlayerPrefsSetUp();
         _dataManager.ShowTimer();
+        SetUpTexts();
+        DisablePlayer();
+        _victoryCanvas.SetActive(true);
+    }
+
+    private void SetUpTexts()
+    {
         _hightScore.text = _dataManager.hightScoreTxt;
         _hightDeathScore.text = _dataManager.minNbDeath.ToString();
         _scoreText.text = _timer.scoreText.text.ToString();
         _scoreDeathText.text = nbDeath.ToString();
-        DisablePlayer();
-        _victoryCanvas.SetActive(true);
-       // Time.timeScale = 0f;
     }
 
     private void DisablePlayer()
@@ -88,4 +101,5 @@ public class GameManager : MonoBehaviour
         _player.GetComponent<PlayerController>().enabled = false;
         _player.GetComponent<PlayerSetUp>().enabled = false;
     }
+    #endregion
 }
