dc41e6c959ca23bab518f5b60ec70d035fad812d
Jado <jade.bouiges@edu.esiee-it.fr>
2024-12-01 17:09:30 +0100

    Change Movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -179,22 +179,22 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W))
         {
-            rb.AddForce((direction * _grappleSpeed) * _speed * Time.deltaTime);
+            rb.AddForce((Vector3.up * 1.5f) * _speed * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.S) && distance < _grappleMaxDistance)
         {
-            rb.AddForce((-Vector3.up * _grappleSpeed) * _speedVertical * Time.deltaTime);
+            rb.AddForce((-Vector3.up * 1.5f) * _speedVertical * Time.deltaTime);
         }
 
 
         if (Input.GetKey(KeyCode.A))
         {
-            rb.AddForce((Vector3.right - direction) * _speed * Time.deltaTime);
+            rb.AddForce((Vector3.right) * _speed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            rb.AddForce((-Vector3.right - direction) * _speed * Time.deltaTime);
+            rb.AddForce((-Vector3.right) * _speed * Time.deltaTime);
         }
 
         _canJump = true;
